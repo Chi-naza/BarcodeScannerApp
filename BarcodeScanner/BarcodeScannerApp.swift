7f132defd28cb64dbb764f4f93135619c01ae1e6
Chi-naza <ugwuokechinazajames@gmail.com>
2024-05-06 20:25:23 +0100

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Chinaza Ugwuoke on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
